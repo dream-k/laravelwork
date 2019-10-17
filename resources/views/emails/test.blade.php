9864de629fac625bffdef6222d8e9213a097f1be
dream-k <topdev.star@gmail.com>
2019-10-18 01:49:59 +0800

    first commit

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Test</title>
+  </head>
+  <body style="margin:0px; background: #f8f8f8; ">
+    <div width="100%" style="background: #f8f8f8; padding: 0px 0px; font-family:arial; line-height:28px; height:100%;  width: 100%; color: #514d6a;">
+      <div style="max-width: 700px; padding:50px 0;  margin: 0px auto; font-size: 14px">
+        <h1>Welcome</h1>
+        <div style="padding: 40px; background: #fff;">
+          
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
